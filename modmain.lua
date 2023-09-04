4b6344775b47ab080923f378255eb34e4e13a751
splorange12 <trentw25@frontier.com>
2023-09-04 19:02:14 -0400

    expanded crafting outside of just torch

--- modmain.lua
+++ modmain.lua
@@ -1,7 +1,19 @@
+local function TryCraft(check)
+    for recname, rec in pairs(GLOBAL.AllRecipes) do
+        if GLOBAL.IsRecipeValid(recname) and rec.placer == nil and rec.sg_state ==
+            nil and GLOBAL.ThePlayer.replica.builder:KnowsRecipe(recname) and
+            GLOBAL.ThePlayer.replica.builder:CanBuild(recname) then
+            if check then return true end
+            GLOBAL.SendRPCToServer(GLOBAL.RPC.MakeRecipeFromMenu, rec.rpc_id)
+            return rec
+        end
+    end
+end
+
 local function BeefaloCancel(inst)
     local beefalo = inst.replica.rider.classified ~= nil and inst.replica.rider.classified.ridermount:value()
     local wx, _, wz = beefalo.Transform:GetWorldPosition()
-    GLOBAL.TheNet:SendRPCToServer(GLOBAL.RPC.MakeRecipeFromMenu, 165) -- torch
+    TryCraft()
     GLOBAL.ThePlayer:DoTaskInTime(0.1,function()
         GLOBAL.TheNet:SendRPCToServer(GLOBAL.RPC.LeftClick, GLOBAL.ACTIONS.WALKTO.code, wx+1,wz+1)
             GLOBAL.ThePlayer:DoTaskInTime(0.08,function()
@@ -9,15 +21,6 @@ local function BeefaloCancel(inst)
         end)
     end)
 end
-
---[[AddPlayerPostInit(function(inst)
-	inst:ListenForEvent("isridingdirty", function(inst)
-		if inst ~= GLOBAL.ThePlayer then return end
-        if inst.AnimState:IsCurrentAnimation("buck") or inst.AnimState:IsCurrentAnimation("buck_pst") then
-            BeefaloCancel(inst)
-        end
-    end)
-end)]]--
 
 AddPlayerPostInit(function(inst)
 	inst:ListenForEvent("isridingdirty", function(inst)
